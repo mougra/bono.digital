2d419f50ceb2e0ec6e7eff5047f8dbb4bd0e7e1c
egor antrop <mougraaa@gmail.com>
2023-12-07 21:05:38 +0400

    feat: validate form

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,67 @@
-// import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
 
-// const navClick = document.querySelector('.navigation__icon-cat');
-// const navOpen = document.querySelector('.nav-inside');
-// const navClose = document.querySelector('.nav-inside__close');
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form-message');
+  form.addEventListener('submit', formSend);
 
-// navClick.addEventListener('click', function () {
-//   navOpen.classList.add('nav-inside_active');
-// });
-// navClose.addEventListener('click', function () {
-//   navOpen.classList.remove('nav-inside_active');
-// });
+  async function formSend(e) {
+    e.preventDefault();
 
-// const searchClick = document.querySelector('.header__search-mob');
-// const searchMobileOpen = document.querySelector('.header__seacrh_mobile');
-// const searchClickClose = document.querySelector('.btn-close-mob');
+    let error = formValidate(form);
 
-// searchClick.addEventListener('click', function () {
-//   console.log('seacrh');
-//   searchMobileOpen.classList.toggle('header__seacrh_mobile-active');
-// });
-// searchClickClose.addEventListener('click', function () {
-//   console.log('seacrh');
-//   searchMobileOpen.classList.remove('header__seacrh_mobile-active');
-// });
+    if (error === 0) {
+      let formData = new FormData(form);
+      const name = formData.get('name');
+      const email = formData.get('email');
+      const message = formData.get('message');
+      form.reset();
+      if (name) {
+        console.log(`name: ${name}`);
+      }
+      if (email) {
+        console.log(`email: ${email}`);
+      }
+      if (message) {
+        console.log(`message: ${message}`);
+      }
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+    let formEmail = document.querySelector('._email');
+
+    formRemoveError(formEmail);
+
+    if (emailTest(formEmail)) {
+      formAddError(formEmail);
+      error++;
+    }
+
+    for (let i = 0; i < formReq.length; i++) {
+      const input = formReq[i];
+      formRemoveError(input);
+
+      if (input.value === '') {
+        formAddError(input);
+        error++;
+      }
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
+});
